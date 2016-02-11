accccbd82c1e8fadb470fa388ea806deb679a1ef
Billy Shen <billy.shen@hetic.net>
2016-02-11 01:02:07 +0100

    [ADD] List song feature

--- app/scripts/controllers/song.js
+++ app/scripts/controllers/song.js
@@ -8,10 +8,22 @@
  * Controller of the jaiyeApp
  */
 angular.module('jaiyeApp')
-  .controller('SongCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('SongCtrl', function ($scope, SongService, Logger) {
+
+      $scope.song = {};
+      $scope.songs = [];
+
+      function loadSongs() {
+
+          SongService.find().then(function(data) {
+            $scope.songs = data;
+            console.log('data', data);
+          }, function(err) {
+            Logger.logError('Impossible de charger les sons');
+            console.log('err',err);
+          });
+      }
+
+      loadSongs();
+
   });
